a8f61c11341b341e7903770b82a836f0c3a19250
Tadiwa Leon Muguta <leonmuguta7@gmail.com>
2022-02-14 12:15:25 +0200

    Started using state to output multiple blogs

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
 
-    const handleClicK = (event) => {
-        console.log("Hello!", event);
-    }
-
-    const handleClickAgain = (name) => {
-        console.log(name);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Home page</h2>
-            <button onClick={handleClicK}>Click Me!</button>
-            <button onClick={() => handleClickAgain("Leon")}>Click me again!</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by: { blog.author }</p>
+                </div>
+            ))}
         </div>
     );
 }
