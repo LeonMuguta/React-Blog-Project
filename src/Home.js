65051edc7b04540d0736d462b5b70e21590223db
Tadiwa Leon Muguta <leonmuguta7@gmail.com>
2022-02-16 12:12:09 +0200

    Added routing to different blogs

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,15 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-            .then(res => {
-                if (!res.ok) {
-                    throw Error("Could not fecth the data");
-                }
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsLoading(false);
-                setError(null);
-            })
-            .catch(err => {
-                setError(err.message);
-                setIsLoading(false);
-            })
-    }, []);
+    const { data, isLoading, error } = useFetch("http://localhost:8000/blogs");
 
     return ( 
         <div className="home">
             {error && <div>{ error }</div>}
             {isLoading && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} />}
+            {data && <BlogList blogs={data} />}
         </div>
     );
 }
