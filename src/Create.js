9c133e3db802cb07c42e980ed1426eb1b2131f6b
Tadiwa Leon Muguta <leonmuguta7@gmail.com>
2022-02-17 13:40:58 +0200

    Adding new blogs function added

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,50 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Create = () => {
+
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("mario");
+    const [isLoading, setIsLoading] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const blog = { title, body, author };
+
+        setIsLoading(true);
+
+        fetch("http://localhost:8000/blogs", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log("New Blog Added!");
+            setIsLoading(false);
+            history.push("/");
+        });
+
+        
+    }
+
     return ( 
         <div className="create">
             <h2>Add a new Blog</h2>
+            <form onSubmit={handleSubmit} >
+                <label>Blog title</label>
+                <input type="text" value={ title } onChange={(e) => setTitle(e.target.value)} required/>
+                <label>Blog body</label>
+                <textarea value={ body } onChange={(e) => setBody(e.target.value)} required></textarea>
+                <label>Blog author</label>
+                <select value={ author } onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="mario">Mario</option>
+                    <option value="leon">Leon</option>
+                </select>
+                {!isLoading && <button>Add blog</button>}
+                {isLoading && <button disabled>Adding blog...</button>}
+            </form>
         </div>
     );
 }
